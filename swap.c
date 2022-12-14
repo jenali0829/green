2e85e0c7c11f909335ba4379c7c30c8defb888a3
jenali0829 <120018077+jenali0829@users.noreply.github.com>
2022-12-14 17:56:02 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main(){
+	int a;
+	int b;
+	
+	printf("Ente value A:");
+	scanf("%d",&a);
+	
+	printf("Enter value B:");
+	scanf("%d",&b);
+
+	printf("A:%d\n",b);
+	printf("B:%d\n",a);
+	
+}
